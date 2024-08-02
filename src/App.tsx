5ffd501a74b7b0cdc352f4628f37858107eb627a
wesleykam <wesley_kam@ucsb.edu>
2024-08-01 20:30:45 -0700

    remove stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Wesley from './sections/wesley/wesley';
 
 const App = () => {
     return (
         <>
             <h1>Teamwork Exercise</h1>
-            <Wesley />
         </>
     );
 };
