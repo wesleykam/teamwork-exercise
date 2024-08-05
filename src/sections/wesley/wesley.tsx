6aa2c584e49ee736bbdaa45a172e98c22ce9d00a
wesleykam <wesley_kam@ucsb.edu>
2024-08-05 12:23:24 -0700

    wk - add comments

--- src/sections/wesley/wesley.tsx
+++ src/sections/wesley/wesley.tsx
@@ -1,18 +1,17 @@
 import { useState } from 'react';
 
-import './wesley.css'
+import './wesley.css';
 
 const wesley = () => {
-    
+    // Create the count state that will be updated when the button is clicked
+    // This will be extremely useful in our project
+    // https://legacy.reactjs.org/docs/hooks-state.htmlZ
     const [count, setCount] = useState(0);
-
-    const onClick = () => {
-        setCount(count + 1);
-    }
 
     return (
         <div className="wesley-section">
             <h2>Wesley's Section</h2>
+            <p>Count: {count}</p>
         </div>
     );
 };
